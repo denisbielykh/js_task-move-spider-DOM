5a8fce6110534c8b372e45b383859caa3e5e2f80
denys <denis.bielykh38@gmail.com>
2025-02-17 12:25:14 +0200

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,10 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 document.addEventListener('click', (e) => {
-  if (!e.target.closest('.wall')) {return};
-
+  if (!e.target.closest('.wall')) {
+    return;
+  };
+  
   const wallContentLeft = wall.offsetLeft + wall.clientLeft;
   const wallContentTop = wall.offsetTop + wall.clientTop;
 
@@ -14,20 +16,26 @@ document.addEventListener('click', (e) => {
     y: null,
   };
 
-  if (e.pageX < (wallContentLeft + spider.clientWidth)) {
+  if (e.pageX < wallContentLeft + spider.clientWidth) {
     coords.x = 0;
-  } else if (e.pageX > (wallContentLeft + wall.clientWidth - spider.clientWidth)) {
+  } else if (
+    e.pageX >
+    wallContentLeft + wall.clientWidth - spider.clientWidth
+  ) {
     coords.x = wall.clientWidth - spider.clientWidth;
   } else {
-    coords.x = e.pageX - wallContentLeft - (spider.clientWidth / 2);
+    coords.x = e.pageX - wallContentLeft - spider.clientWidth / 2;
   }
 
-  if (e.pageY < (wallContentTop + spider.clientHeight)) {
+  if (e.pageY < wallContentTop + spider.clientHeight) {
     coords.y = 0;
-  } else if (e.pageY > (wallContentTop + wall.clientHeight - spider.clientHeight)) {
+  } else if (
+    e.pageY >
+    wallContentTop + wall.clientHeight - spider.clientHeight
+  ) {
     coords.y = wall.clientHeight - spider.clientHeight;
   } else {
-    coords.y = e.pageY - (wallContentTop) - (spider.clientHeight / 2);
+    coords.y = e.pageY - wallContentTop - spider.clientHeight / 2;
   }
 
   spider.style.top = coords.y + 'px';
